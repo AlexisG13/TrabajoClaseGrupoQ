6bcd2db54e88d929c52735a180094a5d4c9b1ef1
andre <andre@DESKTOP-UAT26MT>
2018-05-01 14:46:51 -0600

    Creacion de clase Reserva con sus atributos y atributos heredados

---
+++ src/mansionfoster/Reserva.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mansionfoster;
+
+/**
+ *
+ * @author andre
+ */
+public class Reserva {
+    public boolean estado;
+    public Cliente cliente;
+    public Habitacion habitacion, total, costoPorNoche;
+
+    
+}

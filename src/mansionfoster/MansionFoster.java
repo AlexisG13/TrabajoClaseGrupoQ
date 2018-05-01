21df196f8672d154c538dfe36c9be9215f0d940c
Alexis Gomez <Alexis Gomez@192.168.1.111>
2018-05-01 14:34:06 -0600

    Clase principal :o

---
+++ src/mansionfoster/MansionFoster.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mansionfoster;
+
+/**
+ *
+ * @author Alexis Gomez
+ */
+public class MansionFoster {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}

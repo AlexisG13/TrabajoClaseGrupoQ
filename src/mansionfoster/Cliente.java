044eef65d9a526f80d46c6180fb2aa29a37c83bd
rauli <rauli@DESKTOP-B7A8FBJ>
2018-05-01 14:52:27 -0600

    se agrego la clase cliente

---
+++ src/mansionfoster/Cliente.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mansionfoster;
+
+/**
+ *
+ * @author rauli
+ */
+public class Cliente {
+    Nombre nombre;
+    int DUI;
+    Reserva reserva;
+}

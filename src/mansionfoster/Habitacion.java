4302e7dde8daee4bb8c87d2d1fc4618a15c3c8b0
Karina Mina <Karina Mina@KARINA-MINA>
2018-05-01 14:49:37 -0600

    Clase Habitacion

---
+++ src/mansionfoster/Habitacion.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mansionfoster;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Karina Mina
+ */
+public class Habitacion {
+    public ArrayList<HabitacionSencilla> habitacionSencilla;
+    public ArrayList<HabitacionDoble> habitacionDoble;
+}

2e69dd59db26970598870ce2dd9f250e09278f48
rauli <rauli@DESKTOP-B7A8FBJ>
2018-05-02 17:48:08 -0600

    Se agrego la clase Admin

---
+++ src/mansionfoster/Admin.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mansionfoster;
+
+/**
+ *
+ * @author Raul Calderon <https://github.com/calderonra>
+ */
+public class Admin {
+    
+    public String username;
+    private String password;
+    
+    
+    
+}

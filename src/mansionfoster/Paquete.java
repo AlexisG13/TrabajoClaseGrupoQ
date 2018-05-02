b12ef5a205f43f51f6ffb95d172ec1509d632cfc
Alexis Gomez <Alexis Gomez@192.168.1.111>
2018-05-02 14:55:31 -0600

    Se agrega la clase paquete y las respectivas firmas de sus metodos y atributos. :o

---
+++ src/mansionfoster/Paquete.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mansionfoster;
+
+/**
+ *
+ * @author Alexis Gomez
+ */
+public class Paquete {
+//Servicios que pueden contener los paquetes
+public  boolean desayuno;
+public boolean piscina;
+public boolean servicioHabitacion;
+public boolean minibar;
+public boolean internetIlimitado;
+//Métodos 
+private void EscogePaquete(){
+    
+}
+
+private void AgregarPaquete(){
+    
+}
+
+private void ModPaquete(){
+    
+}
+   
+}

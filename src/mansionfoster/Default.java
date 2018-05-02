2d6a2732447767d353d2c71623c621404c659d69
Karina Mina <Karina Mina@KARINA-MINA>
2018-05-02 14:25:30 -0600

    Clase default con su atributo y las firmas de los metodos

---
+++ src/mansionfoster/Default.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mansionfoster;
+
+/**
+ *
+ * @author Karina Mina
+ */
+public class Default {
+
+    public Reserva reserva;
+
+    public Default() {
+    }
+
+    public Reserva getReserva() {
+        return reserva;
+    }
+
+    public void setReserva(Reserva reserva) {
+        this.reserva = reserva;
+    }
+    
+    public void verReservas(){
+        
+    }
+    
+    public void agregarReservas(){
+        
+    }
+    
+    public void modReserva(){
+        
+    }
+
+}

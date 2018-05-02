8fc855dcc53266082402ebc558f2d3245a8d1b56
andre <andre@DESKTOP-UAT26MT>
2018-05-02 13:57:19 -0600

    Clase Habitacion sencilla pasa a ser clase Check

---
+++ src/mansionfoster/Check.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mansionfoster;
+
+/**
+ *
+ * @author andre
+ */
+public class Check {
+    private int hora;
+    private int fecha;
+}

c05461b7d0d0c0f787235f68b35d3af155fb98db
andre <andre@DESKTOP-UAT26MT>
2018-05-02 14:04:22 -0600

    Clase Reserva con sus atributos set y get

--- src/mansionfoster/Reserva.java
+++ src/mansionfoster/Reserva.java
@@ -10,9 +10,49 @@ package mansionfoster;
  * @author andre
  */
 public class Reserva {
-    public boolean estado;
-    public Cliente cliente;
-    public Habitacion habitacion, total, costoPorNoche;
+    private int numDias, numHabsARes;
+    //public Paquete paquete;
+    private Check checkOut;
+    private Check checkIn;
 
+    public Reserva(int numDias, int numHabsARes, Check checkOut, Check checkIn) {
+        this.numDias = numDias;
+        this.numHabsARes = numHabsARes;
+        this.checkOut = checkOut;
+        this.checkIn = checkIn;
+    }
+
+    public Check getCheckOut() {
+        return checkOut;
+    }
+
+    public Check getCheckIn() {
+        return checkIn;
+    }
+
+    public int getNumDias() {
+        return numDias;
+    }
+
+    public int getNumHabsARes() {
+        return numHabsARes;
+    }
+
+    public void setNumDias(int numDias) {
+        this.numDias = numDias;
+    }
+
+    public void setNumHabsARes(int numHabsARes) {
+        this.numHabsARes = numHabsARes;
+    }
+
+    public void setCheckOut(Check checkOut) {
+        this.checkOut = checkOut;
+    }
+
+    public void setCheckIn(Check checkIn) {
+        this.checkIn = checkIn;
+    }
+    
     
 }

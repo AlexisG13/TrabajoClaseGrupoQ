92b00fa04ca7e4ddf4abcdcb352f9c2a7bd3b1b5
andre <andre@DESKTOP-UAT26MT>
2018-05-03 08:15:30 -0600

    Agregando habitaciones a pisos

--- src/mansionfoster/Piso.java
+++ src/mansionfoster/Piso.java
@@ -13,30 +13,34 @@ import java.util.ArrayList;
  */
 public class Piso {
 
-    public static int nPisos = 10;
-    public static ArrayList<Habitacion> piso = new ArrayList<>();
+    public static int nPisos = 6;
+    public static ArrayList<Habitacion> piso = new ArrayList<>();;
 
     public Piso() {
+        
     }
 
-    public void setHabitacion(ArrayList<Habitacion> habitacion) {
-        this.piso = piso;
-    }
+    //public void setHabitacion(ArrayList<Habitacion> habitacion) {
+      //  this.piso = piso;
+    //}
 
     public static void add() {
-        for (int j = 0; j < nPisos; j++) {
-
-            for (int i = 0; i < 10; i++) {
+        for (int j = 1; j < nPisos+1 ; j++) {
+            System.out.println("Piso: "+ j);
+            for (int i = 1; i < 11; i++) {
                 if (i % 2 == 0) {
-                    piso.add(i, new Habitacion(120, true));
+                    piso.add(new Habitacion(120,true)); 
+                    System.out.println("Habitacion doble #"+i);
                 }
                 if (i % 2 != 0) {
-                    piso.add(i, new Habitacion(100, true));
+                    piso.add(new Habitacion(100, true));
+                    System.out.println("Habitacion sencilla #"+i);
                 }
             }
 
         }
     }
+
  public static void mostrar() {
         for (Habitacion e : piso) {
             System.out.println(e.toString());

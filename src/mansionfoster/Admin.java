6d4f791f463fa60a9da10e31c9fa031582093909
andre <andre@DESKTOP-UAT26MT>
2018-05-02 19:27:42 -0600

    Agregando set y get de admin

--- src/mansionfoster/Admin.java
+++ src/mansionfoster/Admin.java
@@ -13,6 +13,26 @@ public class Admin {
     
     public String username;
     private String password;
+
+    public Admin(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
     
     
     

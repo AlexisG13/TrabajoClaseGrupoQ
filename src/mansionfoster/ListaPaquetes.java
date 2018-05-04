1f86a562065a3785ec8cb11dd0bdb36563749b9b
Alexis Gomez <Alexis Gomez@192.168.1.111>
2018-05-03 18:45:07 -0600

    Agregar paquetes, borrar paquetes y visualizarlos

--- src/mansionfoster/ListaPaquetes.java
+++ src/mansionfoster/ListaPaquetes.java
@@ -14,14 +14,36 @@ public class ListaPaquetes {
     
     public static ArrayList<Paquete> Packs = new ArrayList<>();
     public ListaPaquetes(){};
-    public static void AgregarPack(){
+    public static void IniciarPacks(){
         Packs.add(new Paquete("Premium",true,true,true,true,true));
         Packs.add(new Paquete("Básico",false,true,false,false,true));
     }
     public static void MostrarPacks(){
-        int cont=0;
-        System.out.println(Packs.get(0)+"\n"+Packs.get(1));
+        int i=1;
+        for(Paquete p : Packs){
+            System.out.println(i+"- "+p);
+            i++;
+        }
+    }
+    public static void AgregarPack(){
+        System.out.println("Nombre del paquete");
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        Packs.add(new Paquete(s,true,true,true,true,true));
     }
     
+    public static void ModificarPack(){
+        System.out.println("Elija el número del paquete a modificar");
+        MostrarPacks();
+        Scanner scan = new Scanner(System.in);
+        int s = scan.nextInt();
+        Packs.remove(s-1);
+        
+    }
+    
+    public static void Hacete(){
+        IniciarPacks();
+        MostrarPacks();
+    }
     
 }

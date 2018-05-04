dad430ff41d7b2cc4ac16801015c263b66878155
Karina Mina <Karina Mina@KARINA-MINA>
2018-05-03 20:09:16 -0600

    Menu del sistema administrativo

--- src/mansionfoster/Admin.java
+++ src/mansionfoster/Admin.java
@@ -5,12 +5,15 @@
  */
 package mansionfoster;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /**
  *
  * @author Raul Calderon <https://github.com/calderonra>
  */
 public class Admin {
-    
+
     public String username;
     private String password;
 
@@ -34,6 +37,48 @@ public class Admin {
         this.password = password;
     }
     
-    
-    
+    public void opciones() {
+        System.out.println("******** SISTEMA ADMINISTRATIVO ********");
+        System.out.println("1. Deshabilitar / Habilitar Habitaciones");
+        System.out.println("2. Deshabilitar / Habilitar Pisos");
+        System.out.println("3. Modificar Precios");
+        System.out.println("4. Modificar Paquetes");
+        System.out.println("5. Salir");
+    }
+
+    public void Menu() {
+        Piso habitacion = new Piso();
+        int opcion = 6;
+        Scanner leer = new Scanner(System.in);
+
+        while (opcion != 5) {
+            try {
+                opcion = leer.nextInt();
+                switch (opcion) {
+                    case 1: //Des/Habilitar Habitacion 
+                        Piso.cambiarEstadoHabitacion();
+                        Piso.mostrar();
+                        break;
+                    case 2: //Des/Habilitar Piso
+                        Piso.AgregaPiso();
+                        Piso.mostrar();
+                        break;
+                    case 3: //Modificar Precios
+                        Piso.cambiarPrecioHabitacion();
+                        Piso.mostrar();
+                        break;
+                    case 4: //Modificar Paquetes
+                        break;
+                    case 5:
+                        System.out.println("Adios, Rafael");
+                        break;
+                    default:
+                        System.out.println("Por favor ingrese una opcion valida");
+                }
+            } catch (InputMismatchException e) {
+                System.err.println("Por favor, ingerese un número");
+                leer.nextLine();
+            }
+        }
+    }
 }

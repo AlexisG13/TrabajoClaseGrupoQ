3356512342fe73e417098602808f66624997cf9f
Karina Mina <Karina Mina@KARINA-MINA>
2018-05-04 09:55:39 -0600

    cambio de mensajes

--- src/mansionfoster/Default.java
+++ src/mansionfoster/Default.java
@@ -13,19 +13,20 @@ import java.util.Scanner;
  * @author Karina Mina
  */
 public class Default {
-    
+
     public Default() {
     }
-    
+
     public void opciones() {
-        System.out.println("******** SISTEMA DEFAULT ********");
+        System.out.println("------ SISTEMA DEFAULT ------");
         System.out.println("1. Ver reservas semanales");
         System.out.println("2. Agregar reserva");
         System.out.println("3. Cancelar reserva");
         System.out.println("4. Modificar reserva");
         System.out.println("5. Salir");
+        System.out.print(": ");
     }
-   
+
     public void Menu() {
         int opcion = 6;
         Scanner leer = new Scanner(System.in);
@@ -40,21 +41,21 @@ public class Default {
                         break;
                     case 2: //Agregar reserva
                         //Reserva reserva = new Reserva();
-                       // reserva.Reservar();
+                        // reserva.Reservar();
                         break;
                     case 3: //Cancelar reserva
-                        
+
                         break;
                     case 4: //Modificar reserva
                         break;
                     case 5:
-                        System.out.println("Adios, Rafael");
+                        System.out.println("    Saliendo del Sistema Default");
                         break;
                     default:
-                        System.out.println("Por favor ingrese una opcion valida");
+                        System.out.println("    Por favor ingrese una opción valida");
                 }
             } catch (InputMismatchException e) {
-                System.err.println("Por favor, ingerese un número");
+                System.err.println("    Por favor, ingrese un número");
                 leer.nextLine();
             }
         }
